e112bc4bcfb38c5802510a7207f09439b8fc25e8
Francisco <francisco.cornejo1@mail.udp.cl>
2024-02-16 19:10:43 -0300

    Prueba_FundamentosJavaScript resuelto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,118 @@
-//
 $(document).ready(function () {
-  //   let input = $("#input");
-  //   let boton = $("#boton");
-  //   let msj3 = document.querySelector(".msj3"); //se usa clase porque chocaba con bootstrap
+  //ejercicio 5 validacion de un input
+  let formularioDatos = $("#formularioDatos");
+  formularioDatos.submit(function (event) {
+    let input = $("#input");
+    let idValue;
 
-  //   //boton con metodo(addEventListener) y evento(click) con funcion con nombre(validacion) integrada
-  //   boton5.addEventListener("click", function validacion() {
-  //     let capturarInput5 = input5.value;
-  //     let patronBusqueda = /[a-zA-Z]/gim;
-  //     if (capturarInput5.match(patronBusqueda)) {
-  //       msj3.innerHTML = `palabra permitida: ${capturarInput5}`;
-  //     } else {
-  //       alert("error, palabra no permitida");
-  //     }
-  //   });
+    event.preventDefault();
 
-  $.ajax({
-    url: "https://akabab.github.io/superhero-api/api/all.json",
-    type: "GET",
-    datatype: "json",
-    success: function (DatosAPI) {
-      DatosAPI.forEach(function (element) {
-        let espacio = $("<hr>");
-        $("#api").append(espacio);
+    input.filter(function () {
+      let value = $(this).val();
+      let pattern = /^(?:[1-9]|[1-9][0-9]|[1-9][0-9][0-9]|732)$/;
 
-        //se obtiene ID del heroe
-        const IDHero = $("<p>").text(element.id);
-        $("#api").append(IDHero);
+      if (value.match(pattern)) {
+        idValue = value; // Guarda el valor validado
+      } else {
+        alert("busqueda invalida");
+        $("#input").val("");
+      }
+    });
 
-        //se obtiene nombre del heroe
-        const NombreHero = $("<p>").text(element.name);
-        $("#api").append(NombreHero);
+    if (idValue) {
+      //Realiza la nueva solicitud AJAX con el ID específico
+      $.ajax({
+        url: `https://www.superheroapi.com/api.php/3525635500807579/${idValue}`,
+        type: "GET",
+        datatype: "json",
+        success: function (DatosAPI) {
+          // Limpiar busqueda anterior
+          $("#api").empty();
+          $("#input").val("");
 
-        //se obtiene imagen del heroe
-        const ImagenHero = $("<img>").attr("src", element.images.md);
-        $("#api").append(ImagenHero);
+          let hero = `
+          <h3 class="text-center">SuperHero Encontrado</h3>
+          <div class="card mb-4">
+              <div class="row">
+                  <div class="col-md-3 mx-4 my-3">
+                      <img src="${
+                        DatosAPI.image.url
+                      }" class="card-img" alt="" />
+                  </div>
+                  <div class="col-md-8">
+                      <div class="card-body ml-0 pl-0">
+                          <h5 class="card-title"> <span class="fw-bold">Nombre:</span> ${
+                            DatosAPI.name
+                          } </h5>
+                          <p class="card-text">
+                          <span class="fw-bold">Conexiones: </span>${
+                            DatosAPI.connections["group-affiliation"]
+                          }
+                          </p>
+                          <ul class="list-group">
+                              <li class="list-group-item">
+                                  <em class="fw-bold">Publicado por </em>: ${
+                                    DatosAPI.biography.publisher
+                                  }
+                              </li>
+                              <li class="list-group-item">
+                                  <em class="fw-bold">Ocupación: </em>${
+                                    DatosAPI.work.occupation
+                                  } 
+                              </li>
+                              <li class="list-group-item">
+                              <em class="fw-bold">Primera Aparición: </em>
+                                      ${DatosAPI.biography["first-appearance"]}
+                              </li>
+                              <li class="list-group-item">
+                                  <em class="fw-bold">Altura: </em>${DatosAPI.appearance.height.join(
+                                    " - "
+                                  )}
+                              </li>
+                              <li class="list-group-item">
+                              <em class="fw-bold">Peso: </em>${DatosAPI.appearance.weight.join(
+                                " - "
+                              )}
+                              </li>
+                              <li class="list-group-item">
+                                  <em class="fw-bold">Aliases: </em>${
+                                    DatosAPI.biography.aliases
+                                  }
+                              </li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>`;
+          $("#api").append(hero);
 
-        //se obtiene conexiones del heroe
-        const ConexionesHeroe = $("<p>").text(
-          element.connections.groupAffiliation
-        );
-        $("#api").append(ConexionesHeroe);
+          let Estadisticas = [];
+          for (let key in DatosAPI.powerstats) {
+            Estadisticas.push({ x: key, y: DatosAPI.powerstats[key] });
+          }
 
-        //se obtiene primera publicacion del heroe
-        const PublicacionHero = $("<p>").text(element.biography.publisher);
-        $("#api").append(PublicacionHero);
-
-        //se obtiene ocupacion del heroe
-        const OcupacionHero = $("<p>").text(element.work.occupation);
-        $("#api").append(OcupacionHero);
-
-        //se obtiene superpoder del heroe
-        const PrimeraAparicion = $("<p>").text(
-          element.biography.firstAppearance
-        );
-        $("#api").append(PrimeraAparicion);
-
-        //se obtiene altura del heroe
-        const AlturaHero = $("<p>").text(element.appearance.height);
-        $("#api").append(AlturaHero);
-
-        //se obtiene peso del heroe
-        const PesoHero = $("<p>").text(element.appearance.weight);
-        $("#api").append(PesoHero);
-
-        //se obtiene alianzas del heroe
-        const AliasHeroe = $("<p>").text(element.biography.aliases);
-        $("#api").append(AliasHeroe);
+          let opcion = {
+            title: {
+              text: `Estadísticas de Poder: ${DatosAPI.name}`,
+            },
+            data: [
+              {
+                type: "pie",
+                startAngle: 45,
+                showInLegend: "true",
+                legendText: "{label}",
+                indexLabel: "{label} ({y})",
+                yValueFormatString: "#,##0.#" % "",
+                dataPoints: Estadisticas,
+              },
+            ],
+          };
+          $("#chartContainer").CanvasJSChart(opcion);
+        },
+        error: function () {
+          alert("Error al obtener datos del SuperHero");
+        },
       });
-    },
+    }
   });
 });
